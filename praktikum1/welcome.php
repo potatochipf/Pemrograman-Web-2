45e4a8ff0bd22bdddaafc9d76b75becf9c6ea2c5
Muhamad Ihlal <muhamadihlal906@gmail.com>
2025-02-23 22:00:14 +0700

    Add files via upload

---
+++ praktikum1/welcome.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+    <h3>Selamat Datang di Web pertama PHP saya</h3>
+    <?php
+    $_nama = "Muhamad Ihlal";
+    $_umur = 19;
+    $_berat = 58;
+    $_prodi = "Sistem Informasi";
+    $_kampus = "STT NF";
+    ?>
+    <hr>
+    <?php
+    echo "Nama : $_nama <br> Umur saya $_umur berat badan saya $_berat prodi";
+    echo "Prodi : $_prodi <br> Kampus : $_kampus";
+    ?>
+</body>
+
+</html>
